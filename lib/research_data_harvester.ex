04d0f768e69b27401099f5a4e3894bd25ba6a2eb
Anna Headley <anna.headley@gmail.com>
2020-05-21 09:30:35 -0400

    Generate project with mix new

---
+++ lib/research_data_harvester.ex
@@ -0,0 +1,18 @@
+defmodule ResearchDataHarvester do
+  @moduledoc """
+  Documentation for ResearchDataHarvester.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ResearchDataHarvester.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/research_data_harvester_test.exs
@@ -0,0 +1,8 @@
+defmodule ResearchDataHarvesterTest do
+  use ExUnit.Case
+  doctest ResearchDataHarvester
+
+  test "greets the world" do
+    assert ResearchDataHarvester.hello() == :world
+  end
+end
