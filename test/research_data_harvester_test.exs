5b6e7130f9c1b5423bef3e9fd0bd0b28a9d37c2e
Anna Headley <anna.headley@gmail.com>
2020-05-22 10:17:01 -0400

    Refactor dryad tests mocks, fixtures for consistency

--- test/research_data_harvester_test.exs
+++ test/research_data_harvester_test.exs
@@ -3,28 +3,27 @@ defmodule ResearchDataHarvesterTest do
   import Mock
 
   describe "#get_dryad_records" do
-    test "returns parsed json of Princeton University records" do
-      url = "https://datadryad.org/api/v2/datasets"
-      body = File.read!("test/fixtures/dryad_records_page_1.txt")
-
-      get_mock = fn url, _params, _headers ->
-        {
-          :ok,
-          %HTTPoison.Response{
-            body: body,
-            status_code: 200
-          }
+    def mock_dryad_records("https://datadryad.org/api/v2/datasets", _headers, _options) do
+      body = File.read!("test/fixtures/dryad/dryad_page_1.json")
+      {
+        :ok,
+        %HTTPoison.Response{
+          body: body,
+          status_code: 200
         }
-      end
+      }
+    end
 
-      json =
-        Mock.with_mock HTTPoison, get!: get_mock do
+    test "returns parsed json of Princeton University records" do
+
+      output =
+        Mock.with_mock HTTPoison, get!: &mock_dryad_records/3 do
           ResearchDataHarvester.get_dryad_records()
         end
 
-      assert Map.keys(json) == ["_embedded", "_links", "count", "total"]
-      assert json["count"] == 10
-      assert json["total"] == 33511
+      assert Map.keys(output) == ["_embedded", "_links", "count", "total"]
+      assert output["count"] == 10
+      assert output["total"] == 33511
     end
   end
 
