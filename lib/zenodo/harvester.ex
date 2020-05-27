e9aeac63a90d51caabc2749b5ce1801812034a75
Eliot Jordan <eliotj@princeton.edu>
2020-05-27 16:52:59 -0500

    Simplfy get_zenodo_fields method and convert Enum to Stream

--- lib/zenodo/harvester.ex
+++ lib/zenodo/harvester.ex
@@ -7,35 +7,31 @@ defmodule Zenodo do
 
     def get_zenodo_fields(search_url) do
       ZenodoApiStream.response_pages(search_url: search_url)
-      |> Enum.reduce([], &accumulate_fields/2)
-      |> Enum.uniq
+      |> Stream.flat_map(&get_hits/1)
+      |> Stream.flat_map(&extract_fields/1)
+      |> Stream.uniq
+      |> Enum.to_list
     end
 
     defp parse_zenodo_records(body) do
       get_hits(body)
-      |> Enum.map(fn record -> %{ identifier: "doi:#{record["doi"]}" } end)
-    end
-
-    # map is the whole page response body
-    defp accumulate_fields(map, fields_list) do
-      get_hits(map)
-      |> Enum.reduce(fields_list, fn m, acc -> acc ++ extract_fields(m) end)
+      |> Stream.map(fn record -> %{ identifier: "doi:#{record["doi"]}" } end)
     end
 
     defp extract_fields(value) when is_map(value) do
       Map.keys(value)
-      |> Enum.flat_map(fn key -> extract_fields(key, value[key]) end)
+      |> Stream.flat_map(fn key -> extract_fields(key, value[key]) end)
     end
 
     defp extract_fields(key, value) when is_map(value) do
       Map.keys(value)
-      |> Enum.flat_map(fn x -> extract_fields(x, value[x]) end)
-      |> Enum.map(fn x -> "#{key}:#{x}" end)
+      |> Stream.flat_map(fn x -> extract_fields(x, value[x]) end)
+      |> Stream.map(fn x -> "#{key}:#{x}" end)
     end
 
     defp extract_fields(key, value) when is_list(value) do
       value
-      |> Enum.flat_map(fn x -> extract_fields(key, x) end)
+      |> Stream.flat_map(fn x -> extract_fields(key, x) end)
     end
 
     defp extract_fields(key, value) do
